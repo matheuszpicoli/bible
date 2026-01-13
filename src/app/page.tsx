ebb6e23e8f0d9a036e4cbf1884916aadda698b13
matheuszpicoli <matheuspicoli2011@gmail.com>
2026-01-13 16:25:50 -0300

    Home layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import React from "react"
 import type { IBibleBook, IBibleData, IBibleResponse, IBibleVerse } from "../../types/types"
 
@@ -6,6 +7,9 @@ export default async function Bible(): Promise<React.JSX.Element> {
     const data: IBibleData = await response.json()
     const now: Date = new Date()
     const daySeed: number = (now.getFullYear() * 10000 + (now.getMonth() + 1)) * 100 + now.getDate()
+    const versesImagesPath: Array<string> = Array.from({ length: 22 }, (_: unknown, index: number) => `/assets/ImageForVerseOfTheDay (${index + 1}).jpg`)   
+    const imageIndex: number = daySeed % versesImagesPath.length
+    const dailyImage: string = versesImagesPath[imageIndex]
     const bookIndex: number = daySeed % data.books.length
     const book: IBibleBook = data.books[bookIndex]
     const chapterIndex: number = (daySeed + 1) % book.chapters.length
@@ -34,28 +38,36 @@ export default async function Bible(): Promise<React.JSX.Element> {
         month: "long",
         day: "numeric"
     }).toString()
-  
+
     return (
         <section className="home-page">
             <h2>
                 {saudation(now.getHours())}
             </h2>
-            <div className="card">
-                <div className="card-header">
-                    <p>
-                        {`${currentDate.charAt(0).toUpperCase() + currentDate.slice(1)}.`}
-                    </p>
+            <div className="card-gallery">
+                <div className="card">
+                    <Image 
+                        src={dailyImage} 
+                        alt="Imagem do versículo do dia" 
+                        priority={true}
+                        fill
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    />
+                    <div className="card-content">
+                        <h3 className="card-title">{dailyVerse.book} {dailyVerse.chapter}:{dailyVerse.verse}</h3>
+                        <q className="card-description">
+                            {dailyVerse.text}
+                        </q>
+                        <div className="card-tags">
+                            <button className="tag">Bíblia</button>
+                            <button className="tag">Versículo</button>
+                            <button className="tag">Inspiração</button>
+                        </div>
+                    </div>
                 </div>
-                <div className="card-body">
-                    <q>
-                        {dailyVerse.text}
-                    </q>
-                </div>
-                <div className="card-footer">
-                    <cite>
-                        {dailyVerse.book} {dailyVerse.chapter}:{dailyVerse.verse}
-                    </cite>
-                </div>
+            </div>
+            <div className="current-date">
+                {currentDate.charAt(0).toUpperCase() + currentDate.slice(1)}
             </div>
         </section>
     )
